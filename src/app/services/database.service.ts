7cb9cc45da87c554327124bbd9e57945cc966ecd
fjmartinez <fjmartinezduran@gmail.com>
2023-12-13 11:49:52 +0100

    chache disabled

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -8,13 +8,14 @@ import { enableIndexedDbPersistence } from 'firebase/firestore';
 export class DatabaseService {
     
   constructor(private afs: AngularFirestore) { 
-    enableIndexedDbPersistence(this.afs.firestore)
+    console.error('❌ Caché sin activar ');
+    /* enableIndexedDbPersistence(this.afs.firestore)
       .then(() => {
         console.log('✅ Persistencia habilitada en el índice de la base de datos');
       })
       .catch((error) => {
         console.error('❌ Error al habilitar la persistencia en el índice de la base de datos: ', error);
-      });
+      }); */
   }
 
   public getDatabase(){
